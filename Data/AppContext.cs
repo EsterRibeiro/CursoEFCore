bd2433f766e4c7303b777e7591562872ad028f42
Ester Ribeiro dos Santos <ester.santos@teadit.com>
2020-12-27 17:58:31 -0300

    Classe DbContext

---
+++ Data/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Data
+{
+    public class AppContext : DbContext
+    {
+        //Método de configuração da string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlite("Data Source=c:\\mydb.db;Version=3");
+        }
+    }
+}
